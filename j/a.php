e600cc5c9a315cbe8d7d77e0624025ae9a88aeae
66010914022 <66010914022@msu.ac.th>
2026-02-18 13:28:45 +0700

    a

---
+++ j/a.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>อดิเทพ จำเริญเจือ (อาร์ม)</title>
+</head>
+
+<body>
+<h1>66010914022 อดิเทพ จำเริญเจือ (อาร์ม)</h1>
+<h2> สิ่งที่ชอบ : แมว </h2>
+<img src="1.jpg" width="200">
+<img src="2.jpg" width="355">
+
+</body>
+</html>
