ac67551526b76d77c04c4eb431f6e28b3d10e00b
66010914022 <66010914022@msu.ac.th>
2026-02-18 16:51:26 +0700

    a

--- k/a.php
+++ k/a.php
@@ -7,39 +7,22 @@
 
 <body>
 <h1>งาน k  อดิเทพ จำเริญเจือ (อาร์ม)</h1>
-<?php
-$image = "";
 
-if(isset($_POST['btn1'])){
-    $image = "image1.jpg"; // รูปแรก
+<button onclick="showImage('1.jpg', this)" 
+        style="background-color:green; color:white; padding:10px; border:none;">
+    เปิดรูปที่ 1
+</button>
+
+<button onclick="showImage('2.jpg', this)" 
+        style="background-color:orange; color:white; padding:10px; border:none;">
+    เปิดรูปที่ 2
+</button>
+
+<script>
+function showImage(imgSrc, btn){
+    btn.innerHTML = "<img src='" + imgSrc + "' width='150'>";
 }
-
-if(isset($_POST['btn2'])){
-    $image = "image2.jpg"; // รูปที่สอง
-}
-?>
-
-<!DOCTYPE html>
-<html>
-<
-
-<form method="post">
-    <button type="submit" name="btn1" style="background-color:green; color:white; padding:10px 20px; border:none;">
-        เปิดรูปที่ 1
-    </button>
-
-    <button type="submit" name="btn2" style="background-color:orange; color:white; padding:10px 20px; border:none;">
-        เปิดรูปที่ 2
-    </button>
-</form>
-
-<br>
-
-<?php
-if($image != ""){
-    echo "<img src='$image' width='300'>";
-}
-?>
-
+</script>
+ 
 </body>
 </html>
